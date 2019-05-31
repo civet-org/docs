35a68cdf9724631d8f6be7ebfcec6234b82f3f3b
Aaron Burmeister <aaron.burmeister@ils-gmbh.net>
2019-05-31 13:29:40 +0200

    js.org

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,14 +11,14 @@
 const siteConfig = {
   title: "Civet", // Title for your website.
   tagline: "Painless data integration in your React application",
-  url: "https://civet.hatsu.moe", // Your website URL
+  url: "https://civet.js.org", // Your website URL
   baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  cname: "civet.hatsu.moe",
+  cname: "civet.js.org",
   projectName: "docs",
   organizationName: "civet-org",
   // For top-level user or org sites, the organization is still the same.
